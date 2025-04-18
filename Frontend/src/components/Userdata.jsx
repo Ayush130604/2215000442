252113857048a3e143e4c1b46deb7cd180c73699
Ayush130604 <ayushsharma130604@gmail.com>
2025-04-18 13:52:29 +0530

    new Change

--- Frontend/src/components/Userdata.jsx
+++ Frontend/src/components/Userdata.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 
 const Userdata = ({data ,value}) => {
 
-    async function TopFivedata() {
-        const mostComment =       
-    }
-
   return (
     <>
         <h1> Username = {value}</h1>
